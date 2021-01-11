abd65fbd42a965a7e683ee1ec17cfbf616428c4e
The24thDS <davidsima@windowslive.com>
2021-01-11 19:09:56 +0200

    Initial files

---
+++ bot.js
@@ -0,0 +1,71 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const { join, leave, say } = require('./commands');
+const {
+  checkCommamds,
+  replyToInteraction,
+  acknowledgeInteraction,
+} = require('./utils');
+
+const client = new Discord.Client();
+
+client.on('ready', async () => {
+  checkCommamds(client);
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', async (msg) => {
+  const command =
+    msg.content.substr(0, msg.content.indexOf(' ')) || msg.content;
+  const content = msg.content.substr(msg.content.indexOf(' ') + 1);
+  const voiceChannel = msg.member.voice.channel;
+  let reply = '';
+  switch (command) {
+    case ';join':
+      reply = await join(voiceChannel, client);
+      msg.channel.send(reply);
+      break;
+    case ';say':
+      reply = await say(voiceChannel, client, content);
+      if (reply.length) {
+        msg.channel.send(reply);
+      }
+      break;
+    case ';leave':
+      reply = await leave(voiceChannel, client);
+      msg.channel.send(reply);
+    default:
+      break;
+  }
+});
+
+client.ws.on('INTERACTION_CREATE', async (interaction) => {
+  let reply = '';
+  const guild = await client.guilds.fetch(interaction.guild_id);
+  const voiceChannel = guild.members.resolve(interaction.member.user.id).voice
+    .channel;
+  switch (interaction.data.name) {
+    case 'join':
+      reply = await join(voiceChannel, client);
+      replyToInteraction(client, interaction.id, interaction.token, reply);
+      break;
+    case 'leave':
+      reply = await leave(voiceChannel, client);
+      replyToInteraction(client, interaction.id, interaction.token, reply);
+      break;
+    case 'say':
+      const content = interaction.data.options[0].value;
+      reply = await say(voiceChannel, client, content);
+      if (reply.length) {
+        replyToInteraction(client, interaction.id, interaction.token, reply);
+      } else {
+        acknowledgeInteraction(client, interaction.id, interaction.token);
+      }
+      break;
+
+    default:
+      break;
+  }
+});
+
+client.login(process.env.TOKEN);
---
+++ utils.js
@@ -0,0 +1,95 @@
+const textToSpeech = require('@google-cloud/text-to-speech');
+const Duplex = require('stream').Duplex;
+
+const client = new textToSpeech.TextToSpeechClient();
+
+const synthesizeText = async (text) => {
+  const request = {
+    input: { text },
+    voice: {
+      languageCode: 'en-UK',
+      name: 'en-GB-Wavenet-B',
+      ssmlGender: 'MALE',
+    },
+    audioConfig: { audioEncoding: 'OGG_OPUS' },
+  };
+
+  const [response] = await client.synthesizeSpeech(request);
+  return response.audioContent;
+};
+
+const tts = async (connection, text) => {
+  const buffer = await synthesizeText(text);
+  const stream = new Duplex();
+  stream.push(buffer);
+  stream.push(null);
+  connection.play(stream);
+};
+
+const listCommands = async (client, onlyNames = false) => {
+  let commands = await client.api.applications(client.user.id).commands.get();
+
+  if (onlyNames) {
+    commands = commands.map((command) => command.name);
+  }
+
+  return commands;
+};
+
+const registerGlobalCommand = async (client, commandData) =>
+  await client.api.applications(client.user.id).commands.post({
+    data: commandData,
+  });
+
+const checkCommamds = async (client) => {
+  const globalCommandsData = require('./global-commands-data.json');
+  const registeredCommands = await listCommands(client, true);
+
+  if (globalCommandsData.length > registeredCommands.length) {
+    const nonregisteredCommands = globalCommandsData.filter(
+      ({ name }) => !registeredCommands.includes(name)
+    );
+    console.log('Registering new commands');
+    nonregisteredCommands.forEach(async (commandData) => {
+      const response = await registerGlobalCommand(client, commandData);
+      if (response.id) {
+        console.log(`Registered global command ${commandData.name}`);
+      }
+    });
+  } else {
+    console.log('No new commands to register.');
+  }
+};
+
+const replyToInteraction = (
+  client,
+  interactionId,
+  interactionToken,
+  message
+) => {
+  client.api.interactions(interactionId, interactionToken).callback.post({
+    data: {
+      type: 4,
+      data: {
+        content: message,
+      },
+    },
+  });
+};
+
+const acknowledgeInteraction = (client, interactionId, interactionToken) => {
+  client.api.interactions(interactionId, interactionToken).callback.post({
+    data: {
+      type: 5,
+    },
+  });
+};
+
+module.exports = {
+  tts,
+  listCommands,
+  registerGlobalCommand,
+  checkCommamds,
+  replyToInteraction,
+  acknowledgeInteraction,
+};
